9ed520f3b6503662211ac6dc4c9e77ed2c106de1
Tereza Schwarzová <schwarzova@cngroup.dk>
2025-01-23 09:26:17 +0100

    Add plan with gcps, store them in state (#1)

--- src/components/Plan/store.ts
+++ src/components/Plan/store.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { Anchor, Plan } from '../../types';
+import { Anchor, GCP, Plan } from '../../types';
 import { mockedAnchors, mockedPlans } from '../../mocks/mocks';
 
 type PlanState = {
@@ -13,6 +13,7 @@ type PlanState = {
   fetchAnchors: () => Promise<void>;
   selectedPlanSvgUrl?: string;
   fetchPlanSvgUrl: (planId: string) => Promise<void>;
+  gcps: GCP[];
 };
 
 export const usePlanStore = create<PlanState>((set) => ({
@@ -30,6 +31,7 @@ export const usePlanStore = create<PlanState>((set) => ({
       selectedPlan: undefined,
       anchors: [],
       selectedPlanSvgUrl: undefined,
+      gcps: [],
     }),
   anchors: [],
   fetchAnchors: async () => {
@@ -38,13 +40,34 @@ export const usePlanStore = create<PlanState>((set) => ({
     set({ anchors: mockedAnchors });
     set({ isFetching: false });
   },
+  gcps: [],
   fetchPlanSvgUrl: async (planId) => {
     set({ isFetching: true });
     await new Promise((resolve) => setTimeout(resolve, 1000));
     if (planId === '1') {
       set({ selectedPlanSvgUrl: '/src/assets/floorplan.svg' });
-    } else {
+    } else if (planId === '2') {
       set({ selectedPlanSvgUrl: '/src/assets/floorplan2.svg' });
+    } else {
+      set({ selectedPlanSvgUrl: '/src/assets/floorplan3withGCS.svg' });
+
+      // Parse the SVG and extract metadata
+      const response = await fetch('/src/assets/floorplan3withGCS.svg');
+      const text = await response.text();
+      const parser = new DOMParser();
+      const svgDocument = parser.parseFromString(text, 'image/svg+xml');
+      const metadataElement = svgDocument.querySelector('metadata');
+      const gcpElements = metadataElement?.querySelectorAll('gcp');
+
+      if (gcpElements) {
+        const gcps: GCP[] = Array.from(gcpElements).map((gcp) => ({
+          xSvg: Number(gcp.getAttribute('x_svg')!),
+          ySvg: Number(gcp.getAttribute('y_svg')!),
+          xReal: Number(gcp.getAttribute('x_real')!),
+          yReal: Number(gcp.getAttribute('y_real')!),
+        }));
+        set({ gcps });
+      }
     }
     set({ isFetching: false });
   },
