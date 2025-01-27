32031e706448d94ec58e31de291125aa9baec5ab
Alzbeta Bachronikova <bachronikova@cngroup.dk>
2025-01-27 10:43:16 +0100

    tls coordinates

--- src/components/Viewer/AnchorPoint.tsx
+++ src/components/Viewer/AnchorPoint.tsx
@@ -1,21 +1,50 @@
 // import anchorImg from '../../assets/anchor.png';
+import { useState } from 'react';
 import { ANCHOR_SIZE } from '../../constants/consts';
 import { Anchor } from '../../types';
-import { anchorClass } from './styles';
+import { anchorClass, tooltipClass } from './styles';
 
 type Props = {
   anchor: Anchor;
 };
 
 function AnchorPoint(props: Props) {
+  const [isTooltipVisible, setTooltipVisible] = useState(false);
+
+  const top = props.anchor.y - ANCHOR_SIZE / 2;
+  const left = props.anchor.x - ANCHOR_SIZE / 2;
+
+  function handleMouseOver() {
+    setTooltipVisible(true);
+  }
+
+  function handleMouseOut() {
+    setTooltipVisible(false);
+  }
+
   return (
-    <div
-      className={anchorClass}
-      style={{
-        left: `${props.anchor.x - ANCHOR_SIZE / 2}px`,
-        top: `${props.anchor.y - ANCHOR_SIZE / 2}px`,
-      }}
-    />
+    <>
+      <div
+        className={anchorClass}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        style={{
+          left: `${left}px`,
+          top: `${top}px`,
+        }}
+      />
+      {isTooltipVisible && (
+        <div
+          className={tooltipClass}
+          style={{
+            top: top + 10,
+            left: left + 10,
+          }}
+        >
+          Id: {props.anchor.id}
+        </div>
+      )}
+    </>
   );
 
   // prepared for the image rendering (for debugging it is better to see points)
