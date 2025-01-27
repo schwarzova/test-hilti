0ad72d26aa0c875ec9a0b515a4bcbf3b31f18c2e
Tereza Schwarzová <schwarzova@cngroup.dk>
2025-01-27 17:35:55 +0100

    Resize svg to fit the viewer (#8)

--- src/containers/Plan.tsx
+++ src/containers/Plan.tsx
@@ -26,6 +26,8 @@ function Plan() {
   const selectedPlanSvgUrl = usePlanStore((state) => state.selectedPlanSvgUrl);
   const fetchTags = usePlanStore((state) => state.fetchTags);
   const tags = usePlanStore((state) => state.tags);
+  const svgScale = usePlanStore((state) => state.svgScale);
+  const setSvgScale = usePlanStore((state) => state.setSvgScale);
   const parsedSvgData = usePlanStore((state) => state.parsedSvgData);
   const fetchSidebarTools = useSidebarStore((state) => state.fetchTools);
 
@@ -75,6 +77,8 @@ function Plan() {
           planSvgUrl={selectedPlanSvgUrl}
           planWidth={planRef.current.getBoundingClientRect().width}
           tags={tags}
+          svgScale={svgScale}
+          onSvgScaleSet={setSvgScale}
         />
       ) : (
         <PlanSelection
