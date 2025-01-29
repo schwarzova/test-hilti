1f6fa5cc29fc804e203078b3dcd8ea448d3c287d
Alzbeta Bachronikova <bachronikova@cngroup.dk>
2025-01-29 16:05:01 +0100

    better labels

--- src/components/Viewer/MeasuredReferencePoint.tsx
+++ src/components/Viewer/MeasuredReferencePoint.tsx
@@ -1,10 +1,15 @@
 import { cx } from '../../../styled-system/css';
 import { Point } from '../../types';
-import { measuredPointClass, groundTruthPointClass } from './styles';
+import {
+  measuredPointClass,
+  groundTruthPointClass,
+  measuredPointLabelClass,
+} from './styles';
 
 type Props = {
   point: Point;
   isGroundTruthPoint?: boolean;
+  id?: string;
 };
 
 function MeasuredReferencePoint(props: Props) {
@@ -12,16 +17,26 @@ function MeasuredReferencePoint(props: Props) {
   const left = props.point.x;
 
   return (
-    <div
-      className={cx(
-        measuredPointClass,
-        props.isGroundTruthPoint && groundTruthPointClass,
+    <>
+      <div
+        className={cx(
+          measuredPointClass,
+          props.isGroundTruthPoint && groundTruthPointClass,
+        )}
+        style={{
+          left: `${left}px`,
+          top: `${top}px`,
+        }}
+      />
+      {props.id && (
+        <span
+          className={measuredPointLabelClass}
+          style={{ top: top + 5, left: left + 5 }}
+        >
+          {props.id.split(' ')[2]}
+        </span>
       )}
-      style={{
-        left: `${left}px`,
-        top: `${top}px`,
-      }}
-    />
+    </>
   );
 }
 
