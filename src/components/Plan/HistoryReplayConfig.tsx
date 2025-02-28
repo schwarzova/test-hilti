0b6aa307e93817e25137b1cc621c5fe1920c61bc
BetkaB <a.bachronikova@gmail.com>
2025-02-28 11:25:11 +0100

    Colors (#51)
    * anchor tooltips, tls coordinates
    
    * colors update
    
    * prettier
    
    ---------
    
    Co-authored-by: Alzbeta Bachronikova <bachronikova@cngroup.dk>

--- src/components/Plan/HistoryReplayConfig.tsx
+++ src/components/Plan/HistoryReplayConfig.tsx
@@ -212,6 +212,7 @@ function HistoryReplayConfig() {
                   <Space direction="vertical" size={2}>
                     <ConfigLabel>Time step</ConfigLabel>
                     <Select
+                      style={{ minWidth: '80px' }}
                       size="small"
                       onChange={handleTimeStepChange}
                       defaultValue={HISTORICAL_TIME_STEP}
